1ea2e6f6399dc606e2fb5449d1ced6e11b385b9b
Preston <prestonfransen@gmail.com>
2020-01-22 20:57:36 -0700

    Making the piggy

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-var makeItRain = function() {
+var makePiggysRain = function() {
   //clear out everything
   $('.rain').empty();
 
   var increment = 0;
   var drops = "";
   var backDrops = "";
-
   while (increment < 100) {
+
     //couple random numbers to use for various randomizations
     //random number between 98 and 1
     var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
@@ -15,12 +15,11 @@ var makeItRain = function() {
     //increment
     increment += randoFiver;
     //add in a new raindrop with various randomizations to certain CSS properties
-    drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-    backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+    drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%;"></div>';
+    backDrops += '<div class="drop" style="right: ' + increment + '%;"></div>';
   }
 
   $('.rain.front-row').append(drops);
   $('.rain.back-row').append(backDrops);
-};
-
-makeItRain();
+}
+makePiggysRain();
